a37d5dc6779eabe9ecea3d1638e8a3869c6d74e6
Renato Alisson <poleguin@gmail.com>
2022-07-19 18:12:48 -0300

    Add WeatherResponse.cs

---
+++ Weather.CLI/WeatherResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather.CLI
+{
+    internal class WeatherResponse
+    {
+        public Weather Main { get; set; }
+        public Forecast Sys { get; set; }
+        public string Name { get; set; }
+
+    }
+}
