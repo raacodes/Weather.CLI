95eadc79aeb310a61ac5d34ccca2ec4ec973949a
Renato Alisson <poleguin@gmail.com>
2022-07-19 18:11:41 -0300

    Add Weather.cs

---
+++ Weather.CLI/Weather.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather.CLI
+{
+    internal class Weather
+    {
+        public float Temp { get; set; }
+        public float Temp_min { get; set; }
+        public float Temp_max { get; set; }
+        public float Humidity { get; set; }
+    }
+    internal class Forecast
+    {
+        public float State { get; set; }
+        public string Country { get; set; }
+        public float Date { get; set; }
+    }
+}
